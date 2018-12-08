ea8811ca4b5f76fe3fa3c3b203bf25cc1dec3e10
Sean <xsmpx09@hotmail.com>
2018-12-08 13:08:49 -0500

    update

---
+++ file04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printValue(int x, int y){
+    cout << x << endl;
+    cout << y << endl;
+}
+
+int main(){
+    printValue(10, 20);
+
+    return 0;
+}
